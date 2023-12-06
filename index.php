5e2ce26d0705bb96ab50da385ef0b6cda3cc9c02
itamarpambo <itamarpambo751@gmail.com>
2023-12-06 17:16:52 +0100

    Final

--- index.php
+++ index.php
@@ -18,3 +18,13 @@ $mail->Port = 587;
 
 $mail->setFrom('seu_email@gmail.com', 'Seu Nome');
 $mail->addAddress('destinatario@gmail.com', 'Nome do Destinatário');
+
+$mail->Subject = 'Assunto do Email';
+$mail->Body    = 'Conteúdo do Email em HTML ou texto simples';
+
+try {
+    $mail->send();
+    echo 'Email enviado com sucesso!';
+} catch (Exception $e) {
+    echo "Erro ao enviar o email: {$mail->ErrorInfo}";
+}
