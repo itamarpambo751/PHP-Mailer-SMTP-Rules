76600892c0db9bdde628633150eefd0c5cf35bf6
itamarpambo <itamarpambo751@gmail.com>
2023-12-06 17:01:53 +0100

    Faça as configurações necessárias

--- index.php
+++ index.php
@@ -3,3 +3,15 @@ require './PHPMailer-PHPMailer/src/PHPMailer.php';
 require './PHPMailer-PHPMailer/src/SMTP.php';
 require './PHPMailer-PHPMailer/src/Exception.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true); // Pode lançar exceções
+$mail->isSMTP();
+$mail->Host = 'smtp.seudominio.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'seu_email@seudominio.com';
+$mail->Password = 'sua_senha';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
